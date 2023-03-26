afb3b3552952b7c18aed7279557a50728f5039b6
Mehmet-Altuntas1986 <mehmetscience001@gmail.com>
2023-03-26 15:38:35 +0200

    homaPage elelemnts applied to page object pattern

--- cypress/integration/pageObjects/HomePage.js
+++ cypress/integration/pageObjects/HomePage.js
@@ -1,13 +1,43 @@
 class HomePage{
 
-    getEditBox(){
+getEditBox(){
+    
+      return cy.get("input[name='name']:nth-child(2)")
+}
 
-        return cy.get("input[name='name']:nth-child(2)")
-        }
+
+getTwoWayDataBinding(){
+    return cy.get(':nth-child(4) > .ng-pristine')    
+}
+
+
+getEditBox(){
+return cy.get("input[name='name']:nth-child(2)")
+}
+
+getGender()
+{
+    return cy.get('select')
+}
+
+
+get EntrepreneaurRadioButton(){
+    return cy.get('#inlineRadio3')
+        
+}
+
+
+getShopTab(){
+    return cy.get(':nth-child(2) > .nav-link')
+}
+
+
+
 
 
 }
 
 
 //export class to make it available to use in other files of our project
+//bu class is not related to cypress , that is why , we should inport it in the testpages
 export default HomePage;
