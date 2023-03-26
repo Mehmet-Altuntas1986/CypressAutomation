85187e577f29d8a7d64fe7819434828f4b0a2a06
Mehmet-Altuntas1986 <83958161+Mehmet-Altuntas1986@users.noreply.github.com>
2023-03-26 15:43:02 +0200

    Revert "homaPage elelements applied to page object pattern"

--- cypress/integration/pageObjects/HomePage.js
+++ cypress/integration/pageObjects/HomePage.js
@@ -1,43 +1,13 @@
 class HomePage{
 
-getEditBox(){
-    
-      return cy.get("input[name='name']:nth-child(2)")
-}
+    getEditBox(){
 
-
-getTwoWayDataBinding(){
-    return cy.get(':nth-child(4) > .ng-pristine')    
-}
-
-
-getEditBox(){
-return cy.get("input[name='name']:nth-child(2)")
-}
-
-getGender()
-{
-    return cy.get('select')
-}
-
-
-get EntrepreneaurRadioButton(){
-    return cy.get('#inlineRadio3')
-        
-}
-
-
-getShopTab(){
-    return cy.get(':nth-child(2) > .nav-link')
-}
-
-
-
+        return cy.get("input[name='name']:nth-child(2)")
+        }
 
 
 }
 
 
 //export class to make it available to use in other files of our project
-//bu class is not related to cypress , that is why , we should inport it in the testpages
 export default HomePage;
